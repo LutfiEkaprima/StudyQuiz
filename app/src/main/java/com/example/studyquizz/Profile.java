75e9acca7ff70e00f9408ea89c2389bcabde33d4
Lutfieka <lutfiekj@gmail.com>
2023-05-23 22:39:15 +0700

    Initial commit

--- app/src/main/java/com/example/studyquizz/Profile.java
+++ app/src/main/java/com/example/studyquizz/Profile.java
@@ -1,5 +1,6 @@
 package com.example.studyquizz;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.OnBackPressedCallback;
@@ -10,16 +11,43 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Profile extends Fragment {
 
     long exittime = 0;
+    TextView text1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        text1 = (TextView) view.findViewById(R.id.logout);
+
+        text1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+
+        return view;
     }
+
+    private void logout() {
+        Intent intent = new Intent(getActivity(), Profile.class);
+        startActivity(intent);
+        getActivity().finish();
+    }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
